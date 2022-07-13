c618f098906ac3dcb046e0066a1a99ca737e889d
Aaron Zaks <aaronzaks88@gmail.com>
2022-07-13 06:48:25 -0700

    laid foundation for the shop phase, as of now running shopphase will take a shop, gold amount, and team, and let you purchase one of the listed units given your gold is not zero

--- src/SuperAutoFunc.hs
+++ src/SuperAutoFunc.hs
@@ -1,5 +1,4 @@
 module SuperAutoFunc where
-import Data.Bits (Bits(xor))
 import Data.Maybe
 
 -- eventually, I want to have Fighter be a class, and have each fighter be an instance of that class? maybe not
@@ -31,13 +30,6 @@ calcbattle :: (Maybe Fighter, Maybe Fighter) -> (Maybe Fighter, Maybe Fighter)
 calcbattle (Just f1, Just f2) = calcbattle (simattack (Just f1, Just f2))
 calcbattle x = x
 
--- determine the winner! defunct, I assume that pulling the Just Fighter from the return value of calcbattle would be easier
-winner :: (Maybe Fighter, Maybe Fighter) -> Maybe Fighter
-winner (Just f1, Just f2) = winner (simattack (Just f1, Just f2))
-winner (Just f1, Nothing) = Just f1
-winner (Nothing, Just f2) = Just f2
-winner (Nothing, Nothing) = Nothing
-
 -- some fighters!!
 matchup1 :: (Maybe Fighter, Maybe Fighter)
 matchup1 = pairtomaybe (head (zip team1 team2))
@@ -53,33 +45,36 @@ team1 = [Fighter 3 4, Fighter 4 6, Fighter 8 6, Fighter 2 7]
 team2 :: Team
 team2 = [Fighter 100 100]
 
+shop1 :: Team
+shop1 = [Fighter 1 2, Fighter 1 3, Fighter 2 4, Fighter 5 5, Fighter 1 3]
 
--- an insane function that exists in my head that i want to make
+takejust :: (Maybe a, Maybe a) -> Maybe a
+takejust (Nothing, Just x) = Just x
+takejust (Just x, Nothing) = Just x
+takejust _ = Nothing 
 
-take2 :: (Team, Team) -> (Fighter, Fighter)
-take2 (t1, t2) = (head t1, head t2)
+-- have the front two units duke it out, and return the team that is left.
+updateteams :: (Team, Team) -> (Team, Team)
+updateteams (t1, t2) | isNothing m1 && isNothing m2 = (t1', t2') | isNothing m1 && isJust m2 = (t1', f : t2') | isJust m1 && isNothing m2 = (f : t1', t2') where
+    f1 = head t1
+    f2 = head t2
+    (m1, m2) = (calcbattle . pairtomaybe) (f1, f2)
+    f = (fromJust . takejust) (m1, m2)
+    t1' = drop 1 t1
+    t2' = drop 1 t2
+updateteams _ = ([], [])
 
--- take2 -> pairtomaybe -> calcbattle -> updateteams
--- updateteams (team1, team2) (calcbattle (pairtomaybe (take2 (team1, team2)))) returns the two new teams
-
-updateteams :: (Team, Team) -> (Maybe Fighter, Maybe Fighter) -> (Team, Team)
-updateteams (t1, t2) (Nothing, Nothing) = (drop 1 t1, drop 1 t2)
-updateteams (t1, t2) (Nothing, Just (Fighter a h)) = (drop 1 t1, (Fighter a h) : (drop 1 t2))
-updateteams (t1, t2) (Just (Fighter a h), Nothing) = ((Fighter a h) : (drop 1 t1), drop 1 t2) 
-updateteams _ _ = ([], [])
-
--- full calculates the outcome of an entire battle between the two frontline units
-full :: (Team, Team) -> (Team, Team)
-full (t1, t2) = updateteams (t1, t2) (calcbattle (pairtomaybe (take2 (t1, t2))))
-
+-- this predicate decides when a match is over via asking if there exists an empty list as either team. If so, the match is over, otherwise, play on!
 matchover :: (Team, Team) -> Bool
 matchover ([], _) = True
 matchover (_, []) = True
 matchover _ = False
 
+-- have a match until the match is over, return whats left
 match :: (Team, Team) -> (Team, Team)
-match (t1, t2) = until matchover full (t1, t2)
+match (t1, t2) = until matchover updateteams (t1, t2)
 
+-- take the outcome of a battle and determine who won
 t1won :: (Team, Team) -> Maybe Bool
 t1won ([], []) = Nothing
 t1won ([], _) = Just False
@@ -87,3 +82,41 @@ t1won (_, []) = Just True
 t1won _ = Nothing
 
 -- by applying (t1won . match) to a tuple of teams, we are given the outcome of a match. corresponding: [Just True, Nothing, Just False] <-> [team 1 wins, there is a drew, team 2 wins]
+
+showfighter :: Fighter -> String
+showfighter (Fighter a h) = show a ++ " " ++ show h
+
+shop :: Team -> IO Team
+shop team = do
+    putshop shop1 3 team
+    return [Fighter 1 2]
+-- lets just say that units cost 1 gold for now. 
+putshop :: Team -> Int -> Team -> IO ()
+putshop shop gold team = do
+    putStr "Shop: "
+    putStr (show shop)
+    putStr "      "
+    putStr "Gold: "
+    print gold
+    putStrLn ""
+    putStr "Team: "
+    print team
+
+purchaseunit :: Team -> Team -> IO Team
+purchaseunit shop team = do
+    putStrLn ("Pick a unit to purchase:" ++ show [1..(length shop)])
+    n <- getint
+    return ((shop !! n) : team)
+
+getint :: IO Int
+getint = do
+    n <- getLine
+    putStr "Your new team: "
+    return (read n :: Int)
+
+shopphase :: Team -> Int -> Team -> IO Team
+shopphase _ 0 team = return team
+shopphase shop gold team = do
+    putshop shop gold team
+    let team' = purchaseunit shop team
+    team'
